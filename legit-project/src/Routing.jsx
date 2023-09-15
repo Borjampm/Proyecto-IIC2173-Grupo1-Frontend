885310cf9223461e35e82437968b16049bcb7ca4
Natalia Correal <natalia.correal@uc.cl>
2023-09-15 18:49:02 -0300

    crear lista de empresas con su link respectivo

--- legit-project/src/Routing.jsx
+++ legit-project/src/Routing.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App'
 import Registration from './registration/Registration'
+import CompaniesList from './companies/companies-list';
 
 function Routing() {
     return (
@@ -9,6 +10,7 @@ function Routing() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/registration" element={<Registration />} />
+                <Route path="/companies" element={<CompaniesList />} />
             </Routes>
         </BrowserRouter>
         </>
--- legit-project/src/companies/companies-list.jsx
+++ legit-project/src/companies/companies-list.jsx
@@ -1,34 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react'
 
 function CompaniesList() {
 
-    let companies;     // Variable que almacena los datos de la API para utilizarlo después
+    // let companies;     // Variable que almacena los datos de la API para utilizarlo después
 
     const [msg, setMsg] = useState("");     // Variable que almacena el mensaje de error
 
-    const getCompanies = async() => {      // Envía los datos al backend para hacer efectivo el registro
-        axios.get(`${API_URL}/companies-names`, {
-        }).then((response) => {
-            companies = response.data.companies;
-            setMsg("Empresas obtenidas correctamente");
-        }).catch((error) => {
-            setMsg(`Error al obtener las empresas ${error.response.data.message}`)
-        })
-    }
-    getCompanies()
+    const companies = [                   // Este es el formato en el que se tienen que recuperar los datos de la API
+        {"shortName": "Apple Inc.", "symbol": "AAPL"},
+        {"shortName": "Microsoft Corporation", "symbol": "MSFT"},
+        {"shortName": "Amazon.com, Inc.", "symbol": "AMZN"}
+    ]
 
+    // const getCompanies = async() => {      // Envía los datos al backend para hacer efectivo el registro
+    //     axios.get(`${API_URL}/companies-names`, {
+    //     }).then((response) => {
+    //         companies = response.data.companies;
+    //         setMsg("Empresas obtenidas correctamente");
+    //     }).catch((error) => {
+    //         setMsg(`Error al obtener las empresas ${error.response.data.message}`)
+    //     })
+    // }
+    // getCompanies()                       // Tal vez sea necesario utilizar useEffect() para que se actualice la página  y que se muestre
+ 
     return (
         <>
             <div className="companies-list">
                 <p>{ msg }</p>
                 <div className="companies">
-                    { companies.map((company) => {
-                        <Link to={`/companies/${company.symbol}`}>
-                            <div className="company-list-item">
-                                <p>{company.shortName}</p>
+                    { companies.map(function(company) {
+                        return(
+                            <div key={company.symbol} className="company">
+                                <Link to={`/companies/${company.symbol}`}>
+                                    <p>{ company.shortName }</p>
+                                    <p>{ company.symbol }</p>
+                                </Link>
                             </div>
-                        </Link>
-                    })}
+                        )
+                    })
+
+                    }
                 </div>
                 
                 
