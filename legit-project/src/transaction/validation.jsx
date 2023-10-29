e8d018153587b37e92b6b8f121b2164546e9ae2b
Natalia Correal <natalia.correal@uc.cl>
2023-10-28 21:06:54 -0300

    feat: show pdf

--- legit-project/src/transaction/validation.jsx
+++ legit-project/src/transaction/validation.jsx
@@ -11,6 +11,7 @@ function Validation() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [buyStatus, setBuyStatus] = useState();
     const [msg, setMsg] = useState("");
+    const [PDF, setPDF] = useState();
 
     const handleBuy = async(e) => {
         console.log("token", searchParams.get("token_ws"))
@@ -28,17 +29,26 @@ function Validation() {
     }
 
     const handlePDF = async(e) => {
-        axios.post(`${API_URL}/pdf-serverless-2-demo-lambda_handler`, {
-            group: 1,
+        const requestData = {
+            group: "1",
             user: user.email,
-        }).then((response) => {
+        }
+        
+        axios.post(`${API_URL}/pdf-serverless-2-demo-lambda_handler`,requestData).then((response) => {
             console.log(response);
+            setPDF(response.data.url)
             console.log("good pdf");
         }).catch((error) => {
             console.log(error)
             console.log("bad pdf");
         })    
     }
+
+    const externalURL = 'https://www.example.com';
+
+    const openExternalSite = () => {
+      window.open(PDF, '_blank');
+    };
     // Token
     // Username
 
@@ -60,10 +70,18 @@ function Validation() {
             {buyStatus ? (
                 <>
                     <p>Compra exitosa</p>
-                    <button onClick={() => handlePDF()}>Ver comprobate de compra</button>
+                    <button onClick={() => handlePDF()}>Obtener comprobate de compra</button>
                 </>
             ) : (
-                <p>Compra rechazada</p>
+                <p>:(</p>
+            )}
+
+            {PDF ? (
+                <>
+                    <button onClick={openExternalSite}>Abrir PDF</button>
+                </>
+            ) : (
+                <p>:(((</p>
             )}
             
         </>
